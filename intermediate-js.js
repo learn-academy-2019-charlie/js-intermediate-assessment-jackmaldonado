041e6cb1861847ca2c258cd1a488cc6080a55524
EC2 Default User <ec2-user@ip-172-31-11-253.us-east-2.compute.internal>
2019-06-28 21:04:13 +0000

    finished assessment 3

--- intermediate-js.js
+++ intermediate-js.js
@@ -4,37 +4,74 @@
 
 var originalArray = [1, 2, 3, 4, 5, 6, 7]
 
+please = (arr) => {
+  return arr.reverse(arr)
+}
+console.log(please(originalArray));
 
 // 2. Given the object below, complete the console.log to find specific information:
 
 var bicycle = {
-	type: "Roadbike",
-	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
-	wheels: {
-		count: 2,
-		specs: ["road tires", "AX-7563", "80-115 PSI"],
-		brand: "Trek"
-	}
+  type: "Roadbike",
+  gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
+  wheels: {
+    count: 2,
+    specs: ["road tires", "AX-7563", "80-115 PSI"],
+    brand: "Trek"
+  }
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the PSI:
-console.log()
+console.log(bicycle.wheels.specs[2])
 
 
 // 3. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
 var ourString = "Hello Learn Students!"
 
+letterCounter = (str) => {
+  var count = 0
+  for (i = 0; i < str.length; i++) {
+  //if (str[i].toLowerCase() === "l") {
+    if ((str[i] === "l")  || (str[i] === "L") ){
+      count += 1
+    }
+  }
+  return count
+}
+
+console.log(letterCounter(ourString));
 
 // 4. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-
+function getFib(){
+  var answer = [1,1]
+  for(let i = 3; i <= 10; i++){
+    var n = answer[i - 3] + answer[i -2]
+    answer.push(n)
+  }
+  return answer
+}
+console.log(getFib())
 //5. Write a function called oddChecker that takes in the fullArr and uses a for loop to return a new array of only odd numbers. Copy and paste your code and refactor using ES6 syntax.
 
 var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+
+function oddChecker(arr){
+  var answer = []
+  for(let i = 0; i < arr.length; i++){
+    var element = arr[i]
+    if(Number.isInteger(element) ){
+      if(element % 2 !== 0)
+        answer.push(element)
+    }
+  }
+  return answer
+}
+
+console.log(oddChecker(fullArr))
--- intermediate-js.md
+++ intermediate-js.md
@@ -4,28 +4,49 @@
 
 1. What is the difference between .map() and .filter()?
 
+map will return the same amount of values in an array and filter will filter out the values of an array that you want to take out.
 2. Why would you use object destructuring?
 
+Destructuring is a good way to to clean up code and make it more legible.
 3. What is the difference between var, let, and const?
+
+I don't really understand the difference, I think they all do the same thing.
 
 4. Why is testing important?
 
+helps you make sure your code is running properly.
+
 5. What is a higher order function?
 
+A higher orde function, for example is map and filter. I believe they are much more dynamic than a regular function.
 6. What is the difference between a class and an object?
+
+class is a place to put data and methods, so they can be stored together. An object is inside of a class and can also store data.
 
 7. What did you learn during the group project this week? Please include any additional feedback you may have.
 
+I learned a lot about react but also learned that there is a lot more to learn and study.
 
+I learned that I need to study a lot more and that react is very hard but I think I can get better.
 ### HTML/CSS Review questions: First, try to answer each question on your own then Google the answer to further your knowledge.
 
 1. How do you link a CSS file to your HTML page?
+
+The easiest way to link them is by using the style attribute.
 
 2. What is the difference between a div and a span?
 
 3. What is a CSS class? When should you use an id instead of a class?
 
+
+
 4. Name 4 semantic HTML tags.
+
+<header>
+<button>
+<p>
+<ul>
+<ol>
 
 5. What are three options for creating responsive design?
 
